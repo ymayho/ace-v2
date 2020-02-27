d6b328da5ca8f05ddc6eca7caaf68d182b4d3f88
Kura <ymayho@gmail.com>
2020-02-27 02:37:39 -0500

    Updates

---
+++ precache-manifest.317b73c369bd9e112246a59fa679f4b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260683fe9bfc0122dae3dd67f342bd9d",
+    "url": "/ace-v2/index.html"
+  },
+  {
+    "revision": "d3ebb2bce9f17d0d0f70",
+    "url": "/ace-v2/static/css/main.ec062487.chunk.css"
+  },
+  {
+    "revision": "e4af55c78293e3302c53",
+    "url": "/ace-v2/static/js/2.e9db848d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ace-v2/static/js/2.e9db848d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ebb2bce9f17d0d0f70",
+    "url": "/ace-v2/static/js/main.84b7eb02.chunk.js"
+  },
+  {
+    "revision": "760a66bb681e70c53f74",
+    "url": "/ace-v2/static/js/runtime-main.728c02df.js"
+  },
+  {
+    "revision": "9f24ed5670fb495795ef49e8a4530de0",
+    "url": "/ace-v2/static/media/lut_deutan_medium.9f24ed56.png"
+  },
+  {
+    "revision": "664cd3adeef5f7d55ec5fc612b9e7aa1",
+    "url": "/ace-v2/static/media/lut_protan_medium.664cd3ad.png"
+  },
+  {
+    "revision": "4ab95073c269cbf0823c0f17788104e2",
+    "url": "/ace-v2/static/media/lut_tritan_medium.4ab95073.png"
+  }
+]);
